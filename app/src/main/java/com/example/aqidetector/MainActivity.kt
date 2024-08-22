298b5c172742fe6e88cbcb5367c9cffb70f8047e
Joseph Cho <cho.joseph17@gmail.com>
2024-08-21 21:44:14 -0400

    button that changes background color to green

--- app/src/main/java/com/example/aqidetector/MainActivity.kt
+++ app/src/main/java/com/example/aqidetector/MainActivity.kt
@@ -5,14 +5,21 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.aqidetector.ui.theme.AQIDetectorTheme
 
@@ -38,13 +45,16 @@ fun AQIDetectorApp() {
 
 @Composable
 fun DetectButton(modifier: Modifier) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+    var backgroundColor by remember { mutableStateOf(Color.Black) }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
     ) {
-        Button(onClick = { Log.i("Tag", "the button has been pressed") }) {
+        Button(onClick = { backgroundColor = Color(0xFF00e400) }) {
             Text(text = "I am button")
         }
     }
 }
-
