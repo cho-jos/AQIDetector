52a330481d8c9169d6cd4970dab68eb218622be9
Joseph Cho <cho.joseph17@gmail.com>
2024-08-21 21:10:45 -0400

    simplify, add button that just logs a message

--- app/src/main/java/com/example/aqidetector/MainActivity.kt
+++ app/src/main/java/com/example/aqidetector/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.example.aqidetector
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.aqidetector.ui.screens.MainScreen
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.aqidetector.ui.theme.AQIDetectorTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,8 +22,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AQIDetectorTheme {
-                MainScreen()
+                AQIDetectorApp()
             }
         }
     }
 }
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun AQIDetectorApp() {
+    DetectButton(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
+}
+
+@Composable
+fun DetectButton(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { Log.i("Tag", "the button has been pressed") }) {
+            Text(text = "I am button")
+        }
+    }
+}
+
