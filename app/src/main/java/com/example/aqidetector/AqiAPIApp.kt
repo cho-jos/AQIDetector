bbab1346314c4ec699137130a574f2dac3e97a36
Joseph Cho <cho.joseph17@gmail.com>
2024-08-23 16:05:17 -0400

    preview main app

--- app/src/main/java/com/example/aqidetector/AqiAPIApp.kt
+++ app/src/main/java/com/example/aqidetector/AqiAPIApp.kt
@@ -1,9 +1,11 @@
 package com.example.aqidetector
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.aqidetector.models.AqiViewModel
 import com.example.aqidetector.screens.MainAqiScreen
 
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun AqiAPIApp() {
     MainAqiScreen(viewModel = AqiViewModel())
